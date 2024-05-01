d9860d13ee691c284ccc00a1726cb73ec8cc2425
juamaya <marinamaya@hotmail.es>
2024-05-01 17:11:59 +0200

    deploy 3

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,5 +7,7 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   site: 'https://juamaya.github.io',
+  base: '/web-astro-blog',
+
   integrations: [mdx(), sitemap(), react() ]
 });
